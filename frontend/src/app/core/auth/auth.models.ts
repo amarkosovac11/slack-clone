593ef0d2fb15f8eb1640139a975e11081cf977a6
amarkosovac11 <amarkosovac11@gmail.com>
2026-08-02 14:23:18 +0200

    Add JWT interceptor and auth guard

--- frontend/src/app/core/auth/auth.models.ts
+++ frontend/src/app/core/auth/auth.models.ts
@@ -22,3 +22,12 @@ export interface LoginResponse {
   expiresIn: number;
   user: UserResponse;
 }
+
+export interface ApiErrorResponse {
+  timestamp: string;
+  status: number;
+  error: string;
+  message: string;
+  path: string;
+  validationErrors: Record<string, string> | null;
+}
