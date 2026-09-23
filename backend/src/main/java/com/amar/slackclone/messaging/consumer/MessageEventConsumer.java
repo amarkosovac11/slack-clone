8616e889d4c0844ab0c5fa85bf76e18c0d6f1cbf
amarkosovac11 <amarkosovac11@gmail.com>
2026-09-23 18:43:32 +0200

    Messaging consumer added

--- backend/src/main/java/com/amar/slackclone/messaging/consumer/MessageEventConsumer.java
+++ backend/src/main/java/com/amar/slackclone/messaging/consumer/MessageEventConsumer.java
@@ -1,5 +1,14 @@
 package com.amar.slackclone.messaging.consumer;
 
+import com.amar.slackclone.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MessageEventConsumer {
-    
+
+    @RabbitListener(queues = RabbitMQConfig.MESSAGE_QUEUE)
+    public void receive(String message) {
+        System.out.println("Primljena RabbitMQ poruka: " + message);
+    }
 }
