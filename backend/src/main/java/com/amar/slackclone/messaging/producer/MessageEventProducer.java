44278f0a2ab7b32db27aceaaf5742aef12644a27
amarkosovac11 <amarkosovac11@gmail.com>
2026-09-24 19:09:42 +0200

    Message event producer updated

--- backend/src/main/java/com/amar/slackclone/messaging/producer/MessageEventProducer.java
+++ backend/src/main/java/com/amar/slackclone/messaging/producer/MessageEventProducer.java
@@ -1,29 +1,23 @@
 package com.amar.slackclone.messaging.producer;
 
 import com.amar.slackclone.config.RabbitMQConfig;
+import com.amar.slackclone.messaging.event.MessageCreatedEvent;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
-
-import jakarta.annotation.PostConstruct;
 
 @Component
 public class MessageEventProducer {
 
     private final RabbitTemplate rabbitTemplate;
 
-    @PostConstruct
-    public void testSend() {
-        send("RabbitMQ radi!");
-    }
-
     public MessageEventProducer(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
 
-    public void send(String message) {
+    public void send(MessageCreatedEvent event) {
         rabbitTemplate.convertAndSend(
                 RabbitMQConfig.MESSAGE_EXCHANGE,
                 RabbitMQConfig.MESSAGE_ROUTING_KEY,
-                message);
+                event);
     }
 }
